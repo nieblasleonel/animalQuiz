73f6f5fd7e2c187bec2be15751e7a1612dd2d1ef
Nieblas <52221271+nieblasleonel@users.noreply.github.com>
2024-01-22 16:50:38 -0600

    Update question.js

--- src/question.js
+++ src/question.js
@@ -2,79 +2,135 @@
 export default [
     {
       id: 'q1',
-      text: 'Which of the following definitions best describes React.js?',
+      text: 'Rabbits are born blind. True or false?',
       answers: [
-        'A library to build user interfaces with help of declarative code.',
-        'A library for managing state in web applications.',
-        'A framework to build user interfaces with help of imperative code.',
-        'A library used for building mobile applications only.',
+        'True',
+        'False',
       ],
    
     },
     {
       id: 'q2',
-      text: 'What purpose do React hooks serve?',
+      text: 'How many legs does a spider have?',
       answers: [
-        'Enabling the use of state and other React features in functional components.',
-        'Creating responsive layouts in React applications.',
-        'Handling errors within the application.',
-        'Part of the Redux library for managing global state.',
-      ],
-      
+        '8 legs',
+        '6 legs',
+        '4 legs',
+        '5 legs',
+      ],      
     },
     {
       id: 'q3',
-      text: 'Can you identify what JSX is?',
+      text: 'What is the fastest land animal in the world?',
       answers: [
-        'A JavaScript extension that adds HTML-like syntax to JavaScript.',
-        'A JavaScript library for building dynamic user interfaces.',
-        'A specific HTML version that was explicitly created for React.',
-        'A tool for making HTTP requests in a React application.',
-      ],
-      
+        'The cheetah',
+        'The kangaroo',
+        'The lion',
+      ],      
     },
     {
       id: 'q4',
-      text: 'What is the most common way to create a component in React?',
+      text: 'Which is the tallest bird all over the world?',
       answers: [
-        'By defining a JavaScript function that returns a renderable value.',
-        'By defining a custom HTML tag in JavaScript.',
-        'By creating a file with a .jsx extension.',
-        'By using the "new" keyword followed by the component name.',
-      ],
-      
+        'The ostrich',
+        'The stork',
+      ],      
     },
     {
       id: 'q5',
-      text: 'What does the term "React state" imply?',
+      text: 'What are young pigs called?',
       answers: [
-        'An object in a component that holds values and may cause the component to render on change.',
-        'The lifecycle phase a React component is in.',
-        'The overall status of a React application, including all props and components.',
-        'A library for managing global state in React applications.',
-      ],
-      
+        'Piglets',
+        'Piggies',        
+      ],      
     },
     {
       id: 'q6',
-      text: 'How do you typically render list content in React apps?',
+      text: 'What type of animal is a poodle?',
       answers: [
-        'By using the map() method to iterate over an array of data and returning JSX.',
-        'By using the for() loop to iterate over an array of data and returning JSX.',
-        'By using the forEach() method to iterate over an array of data and returning JSX.',
-        'By using the loop() method to iterate over an array of data and returning JSX.',
-      ],
-      
+        'Dog',
+        'Cat',
+      ],      
     },
     {
       id: 'q7',
-      text: 'Which approach can NOT be used to render content conditionally?',
+      text: 'How can turtles breathe?',
       answers: [
-        'Using a the #if template syntax.',
-        'Using a ternary operator.',
-        'Using the && operator.',
-        'Using an if-else statement.',
-      ],
-      
+        'Through their butt',
+        'Through their mouth',        
+      ],      
     },
+    {
+      id: 'q8',
+      text: 'Which is the animal that cannot jump?',
+      answers: [
+        'Elephants',
+        'Pandas',        
+      ],      
+    },
+    {
+      id: 'q9',
+      text: 'Which mammal can fly?',
+      answers: [
+        'Bats',
+        'Ducks',                
+      ],      
+    },
+    {
+      id: 'q10',
+      text: 'Which animal has 10 hearts?',
+      answers: [
+        'Earthworm',
+        'Octopus',                
+      ],      
+    },
+    {
+      id: 'q11',
+      text: 'what is the fastest creature in the animal kingdom?',
+      answers: [
+        'Peregrine Falcon',
+        'Cheetah',
+        'Horsefly',
+        'Black Marlin'                
+      ],      
+    },
+    {
+      id: 'q12',
+      text: 'The tiniest animal with a backbone is a what?',
+      answers: [
+        'Frog',
+        'Lizard',
+        'Fish'               
+      ],      
+    },
+    {
+      id: 'q13',
+      text: 'Growing up to 59 feet (18 meters) long, which is the world’s largest living fish?',
+      answers: [
+        'Whale shark',
+        'Marlin',  
+        'Manta ray',     
+      ],      
+    },
+    {
+      id: 'q14',
+      text: 'Which of these “fish” is actually a fish?',
+      answers: [
+        'Swordfish',
+        'Starfish',  
+        'Jellyfish',     
+      ],      
+    },
+    {
+      id: 'q15',
+      text: 'What gives flamingos their pink color?',
+      answers: [
+        'Eating shrimp',
+        'Reaction to sunlight',  
+        'Dominat genes',   
+      ],      
+    },
+  
   ];
+
+  
